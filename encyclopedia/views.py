ddf2165ef87e8b8110ebc6d9b45d3612dbbe721c
Amin Farjadi <amin.farjadi@outlook.com>
2020-07-05 15:26:09 +0100

    Entry Page & Index Page specs satisfied

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown
+
 
 def index(request):
+    entries = util.list_entries()
+    hrefs = [f"wiki/{entry}" for entry in entries]
+    entries_info = zip(entries, hrefs)
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries_info": entries_info
     })
+
+def title(request, title):
+    
+    # Retrieve the markdown format of the title
+    md = util.get_entry(title)
+    if not md:
+        return render(request, "encyclopedia/error.html", {"msg": "Title not found"})
+
+    entries = markdown.markdown(md)
+    return render(request, "encyclopedia/title.html", {
+        "title": title,
+        "entries": entries
+        })
 
