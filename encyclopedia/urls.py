945a31582cd24d4cd9030c3c045f24cea5f84bc5
Amin Farjadi <amin.farjadi@outlook.com>
2020-07-07 19:14:02 +0100

    New Page spec satisfied

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.title, name="title"),
-    path("search", views.search, name="search")
+    path("search", views.search, name="search"),
+    path("page_submission", views.page_submission, name="page_submission"),
+    path("page_creation", views.page_creation, name="page_creation"),
+    path("edit/<str:title>", views.edit, name="edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import util
 import markdown
 from django import forms
 import re
 
+class NewPage(forms.Form):
+    title = forms.CharField(label="Page Title")
+    entry = forms.CharField(widget=forms.Textarea)
 
-def index(request, entries = util.list_entries()):
+class Edit(forms.Form):
+    title = forms.CharField(label="Page Title")
+    entry = forms.CharField(widget=forms.Textarea value="this")
+
+
+def index(request, entries=util.list_entries()):
     hrefs = [f"wiki/{entry}" for entry in entries]
     entries_info = zip(entries, hrefs)
     return render(request, "encyclopedia/index.html", {
@@ -36,10 +44,11 @@ def search(request):
     found = []
 
     # searched title is in wiki
-    r = re.compile(f"{search}", re.IGNORECASE)
+    regex = r"\b"+f"{search}"+r"\b"
+    r = re.compile(regex, re.IGNORECASE)
     found = list(filter(r.match, entries))
     if found:
-        return title(request, title=found[0])
+        return redirect('title', title=found[0])
 
     # searched title partially matched entries
     r = re.compile(rf".*{search}.*", re.IGNORECASE)
@@ -48,3 +57,56 @@ def search(request):
         return index(request, entries=found)
 
     return render(request, "encyclopedia/error.html", {"msg": "Title not found"})
+
+def page_submission(request):
+    """Page consisting of a form that the user can use to create a new page"""
+
+    return render(request, "encyclopedia/page_submission.html", {
+        "form": NewPage,
+    })
+
+def page_creation(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/error.html", {"msg": "Page cannot be accessed"})
+
+    form = NewPage(request.POST)
+    if form.is_valid():
+        page_title = form.cleaned_data["title"]
+        page_entry = form.cleaned_data["entry"]
+
+    # check if page with same title exists
+    r = re.compile(f"{page_title}", re.IGNORECASE)
+    found = list(filter(r.match, util.list_entries()))
+
+    if found:
+        return render(request, "encyclopedia/error.html", {"msg": "Page exists"})
+
+    # make the page
+    with open(rf'entries/{page_title}.md', 'w') as file:
+        file.write(page_entry)
+        
+    return redirect('title', title=page_title)
+    
+def edit(request, title):
+    return render(request, "encyclopedia/edit.html", {
+        "form": Edit
+    })
+
+def update(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/error.html", {"msg": "Page cannot be accessed"})
+
+    form = Edit(request.POST)
+
+    if form.is_valid:
+        title = form.cleaned_data["title"]
+        entry = form.cleaned_data["entry"]
+
+    # edit the page
+    with open(rf'entries/{title}.md', 'w') as file:
+        file.write(page_entry)
+    
+    return redirect('title', title=title)
+
+
+    
